858fa4129cf05b5b0d5e2c3d7822384533856c8f
darkye07 <miguelramossantos200011@gmail.com>
2026-05-11 11:58:44 -0300

    RPG
    ainda inacabado

---
+++ src/Arena.java
@@ -0,0 +1,22 @@
+
+
+public class Arena {
+    public static void main(String[] args) {
+
+        Personagem[] Heroi = {
+                new Goblin("Tom Morello", 120, "Perfurante", "Adaga");
+                new Vampiro("Dio", 300, "Sangramento", "presas Reforçadas com Mercúrio"),
+                new Anjo("Edu Falaschi", 7000, "Sagrado", "Livro Sagrado")
+        };
+
+        Personagem[] Inimigo = {
+                new Fantasma("Haunter", 99, "Paranoia", "Ectoplasma"),
+                new Jack("Rudeus", 1000, "Magico", "Cajado D'água"),
+                new Humano("Griffith, O Falcão Branco", 10, "Perfurante", "Rapieira"),
+                new Demonio("Femto, O falcão Negro", 9999, "Demoniaco", "Garras")
+        };
+
+
+
+    }
+}
